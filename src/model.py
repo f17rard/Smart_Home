aa8272e1b046efe78a5bc6d10baef75b65a4e582
Gabriel <gabomendua@gmail.com>
2026-06-19 11:38:33 -0600

    fix de revisión

--- src/model.py
+++ src/model.py
@@ -11,10 +11,10 @@ class TarjetaRed:
 @dataclass
 class Dispositivo(ABC):
     name: str
-    bateria = 100
+    bateria: float = 100.0
     
     @abstractmethod
-    def _consumo_energia(energia: int):
+    def _consumo_energia(self, energia: float):
         pass
 
 class LuzInteligente(Dispositivo):
@@ -25,34 +25,44 @@ class LuzInteligente(Dispositivo):
     
 class AireAcondicionado(Dispositivo):
     
-    def _consumo_energia(self, energia):
-        self.bateria -= self.bateria*energia/1000
+    def _consumo_energia(self, temperatura):
+        self.bateria = self.bateria*temperatura/1000
         return self.bateria
+    
+# --------------Central HUB ----------------------------
 
 class CentralHub:
     def __init__(self, name: str, room: str, direc_mac:str, frecu_ghz: float):
         self.name = name
         self.room = room
-        self.__dispositivos = []
+        self._dispositivos = []
         self.TarjetaR = TarjetaRed(direc_mac, frecu_ghz)
-        self.Cbateria = 100
     
     def Añadir_dispositivo(self, dispositivo):
-        if len(self.__dispositivos) >= 4:
+        if len(self._dispositivos) >= 4:
             raise ValueError("Capacidad de la central agotada.")
-        self.__dispositivos.append(dispositivo)
+        self._dispositivos.append(dispositivo)
         
-    def iniciar_ciclo(self, energia: int):
-        consumo = 0
-        for dispo in self.__dispositivos:
-            consumo = dispo._consumo_energia(energia)
-            self.Cbateria -= consumo/len(self.__dispositivos)
-        
-        print(consumo)
+    def iniciar_ciclo(self, energia: float):
+        promedioBateria = 0
+        for dispo in self._dispositivos:
+            promedioBateria += dispo._consumo_energia(energia)
+        promedioBateria /= len(self._dispositivos)
+        print(promedioBateria)
             
-    def lista_dispositivos(self):
-        for dispositivo in self.__dispositivos:
-            print(dispositivo.name)
+    def desvincular_dispositivo(self, name):
+        if not self._dispositivos:
+            print("La lista está vacia")
+            return
+        for d in self._dispositivos:
+            if(d.name == name):
+                self._dispositivos.remove(d)
+                return
+        print(f"Dispositivo {name} no encontrado")
+            
+    @property
+    def dispositivos(self):
+        return tuple(self._dispositivos)
         
     
 
@@ -69,4 +79,4 @@ CH01.Añadir_dispositivo(LI02)
 CH01.Añadir_dispositivo(LI03)
 CH01.Añadir_dispositivo(AC01)
 
-CH01.iniciar_ciclo(50)
+CH01.iniciar_ciclo(45)
